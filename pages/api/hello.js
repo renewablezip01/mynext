8c23e31d48dcb2efa113794d5a6b0987ec3be8f4
Xelian <renewablezip01@gmail.com>
2022-01-11 19:45:23 +0100

    Add ISR to index page

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+let nummy = 0;
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  nummy += 1;
+  res.status(200).json({ nummy })
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({number}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! - {number}</a>
         </h1>
 
         <p className={styles.description}>
@@ -66,4 +66,16 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  console.log(process.env.DOMAIN);
+const js = await fetch(process.env.DOMAIN + "/api/hello");
+const awaited = await js.json();
+return {
+  props: {
+    number: awaited.nummy,
+  },
+  revalidate: 5,
+}
 }
